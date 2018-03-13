d8212b96312d6b28576c52cce07e2c72791d9d55
KyNguyenNgoc <kylk97@gmail.com>
2018-03-13 15:56:27 +0700

    Add files via upload

--- 1.c
+++ 1.c
@@ -5,22 +5,25 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	int numtasks, rank, sendcount, recvcount, source;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 	// Sum the numbers locally
-	int local_sum = world_rank + 1;
+	int local_sum = rank + 1;
 
 	// Print the random numbers on each process
 	printf("Local sum for process %d - %f\n",
-	       world_rank, local_sum);
+	       rank, local_sum);
 
 	// Reduce all of the local sums into the global sum
 	int global_sum;
-	MPI_Reduce(&local_sum, &global_sum, 1, MPI_FLOAT, MPI_SUM, 0,
+	MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0,
 	           MPI_COMM_WORLD);
 
 	// Print the result
-	if (world_rank == 0) {
-	  printf("Total sum = %d\n", global_sum);
+	if (rank == 0) {
+	  printf("Total sum = %d\nx", global_sum);
 	}
 
 	/* code */
